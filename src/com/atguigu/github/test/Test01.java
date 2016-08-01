2a71583630e9dc415682f45b7a83864d844fe75a
zhangjing <hubei218@126.com>
2016-08-02 01:25:17 +0800

    2
    2

--- src/com/atguigu/github/test/Test01.java
+++ src/com/atguigu/github/test/Test01.java
@@ -6,6 +6,7 @@ public class Test01 {
 		System.out.println("hello github 服务器 ！ ");
 		System.out.println("hello github 服务器02 ！ ");
 			System.out.println("github 回复 01！ ");
+			System.out.println("github 回复 02！ ");
 	}
 
 }
