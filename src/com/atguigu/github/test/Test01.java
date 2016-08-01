68289f2bb5ca4b315f23aa86b1402032c4f23991
zhangjing <hubei218@126.com>
2016-08-02 01:19:29 +0800

    github 回复
    回复

--- src/com/atguigu/github/test/Test01.java
+++ src/com/atguigu/github/test/Test01.java
@@ -5,6 +5,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("hello github 服务器 ！ ");
 		System.out.println("hello github 服务器02 ！ ");
+			System.out.println("github 回复 01！ ");
 	}
 
 }
